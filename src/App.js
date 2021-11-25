be1cd41d987e9d684d689508ee7cd37651985f8b
Viktor Poklad <3d.poklad@gmail.com>
2021-11-25 12:23:50 +0200

    делает апп отзывчивой

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PublicRoutes from './components/routes/PublicRoutes';
 import PrivateRoutes from './components/routes/PrivateRoutes';
 import LoginForm from './components/Forms/LoginForm';
 import RegisterForm from './components/Forms/RegisterForm';
-import AppBar from './components/Appbar/AppBar';
+import AppBar from './components/AppBar/AppBar';
 import Home from './pages/Home';
 import Contacts from './pages/Contacts';
 import { useGetUserQuery } from './services/phoneBookAPI';
@@ -47,6 +47,10 @@ export default function App() {
         <Route
           path="/contacts"
           element={<PrivateRoutes isAuth={isAuth} component={Contacts} />}
+        />
+        <Route
+          path="*"
+          element={<PublicRoutes isAuth={isAuth} component={Home} />}
         />
       </Routes>
     </div>
